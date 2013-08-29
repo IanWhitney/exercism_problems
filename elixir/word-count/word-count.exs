bbe98bc2b4080f3c40f0477534c9204038174f12
Ian Whitney <whit0694@umn.edu>
2013-08-28 19:55:31 -0500

    Cleaned up implementation

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -2,27 +2,21 @@ defmodule Words do
   def count(phrase) do
     word_list = Phrase.to_word_list(phrase)
 
-    HashDict.new(
-      word_list, fn(word) -> 
-        {word, Enum.count(
-          word_list, fn(x) -> 
-            (x == word) 
-          end)
-        } 
-      end
-    )
+    HashDict.new(word_list, fn(word) ->
+      {word, _times_in_list(word_list, word)} 
+    end)
+  end
+
+  defp _times_in_list(list, word) do
+    Enum.count(list, fn(x) -> (x == word) end) 
   end
 end
 
 defmodule Phrase do
   def to_word_list(phrase) do
-    Phrase.to_list(phrase)
+    Regex.split(%r/\W+/, phrase)
       |> List.downcase
       |> List.compact
-  end
-
-  def to_list(phrase) do
-    Regex.split %r/\W+/, phrase
   end
 end
 
