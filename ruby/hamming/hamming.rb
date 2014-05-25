498d03c4bf56b089ac58f32fa97ebdc5b9485170
Ian Whitney <whit0694@umn.edu>
2014-05-24 22:33:36 -0500

    zip short to long to avoid string length problems
    If you zip a longer array into a shorter array, the excess elements in
    the longer array are discarded.
    
    Using this we can avoid problems where one string is longer than the
    other. Though it's a little awkward now, it's still better than the
    previous code.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,10 +2,8 @@ class Hamming
   def self.compute(a,b)
     a = a.split(//)
     b = b.split(//)
-    max = (a.count < b.count) ? a.count : b.count
-    a = a[0,max]
-    b = b[0,max]
-    y = a.zip(b)
+    main = (a.count < b.count) ? [a,b] : [b,a]
+    y = main[0].zip(main[1])
     y.inject(0) {|ret, h| ret += 1 if h.first != h.last; ret}
   end
 end
