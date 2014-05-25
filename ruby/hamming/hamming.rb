8d5d0e35caaa2c1cf6fcc2f2523faea06a6b3d0e
Ian Whitney <whit0694@umn.edu>
2014-05-25 09:59:08 -0500

    Encapsulate the strand behaviors in Strand

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,27 @@
 class Hamming
   def self.compute(a,b)
-    a = a.split(//)
-    b = b.split(//)
+    a = Strand.parse(a)
+    b = Strand.parse(b)
     main = (a.count < b.count) ? [a,b] : [b,a]
     y = main[0].zip(main[1])
     y.inject(0) {|ret, h| ret += 1 if h.first != h.last; ret}
+  end
+end
+
+class Strand
+  include Enumerable
+
+  attr_accessor :collection
+
+  def self.parse(strand_string)
+    self.new(strand_string)
+  end
+
+  def initialize(strand_string)
+    self.collection = strand_string.split(//)
+  end
+
+  def each(&block)
+    collection.each(&block)
   end
 end
