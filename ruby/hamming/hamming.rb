9114fc505295d0c92001724752a1626c770bb19a
Ian Whitney <whit0694@umn.edu>
2014-05-25 10:56:37 -0500

    Strands manipulation now in Strands object

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,20 +1,36 @@
 class Hamming
   def self.compute(a,b)
-    a = Strand.parse(a)
-    b = Strand.parse(b)
-    y = self.combine(self.sort(a,b))
+    y = Strands.new(
+      Strand.parse(a),
+      Strand.parse(b)
+    ).sort.combine
     y.inject(0) {|ret, h| ret += 1 if h.first != h.last; ret}
+  end
+end
+
+class Strands
+  include Enumerable
+
+  def initialize(*strands)
+    self.collection = strands
+  end
+
+  def each(&block)
+    collection.each(&block)
+  end
+
+  def combine
+    collection[0].zip(collection[1])
+  end
+
+  def sort
+    collection.sort_by! { |strand| strand.length }
+    self
   end
 
   private
 
-  def self.combine(sorted_strands)
-    sorted_strands[0].zip(sorted_strands[1])
-  end
-
-  def self.sort(strand_one, strand_two)
-    [strand_one,strand_two].sort_by { |strand| strand.length }
-  end
+  attr_accessor :collection
 end
 
 class Strand
