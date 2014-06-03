6e469e2e861ea148c6e9c2fce7f1aa85b7f6249e
Ian Whitney <whit0694@umn.edu>
2014-06-03 14:11:28 -0500

    Remove duplicate tests
    All these tests do the same thing. Condense down to just one test that
    uses a random date, just in case some weird date combos cause problems.
    
    And I like random test inputs because they help show that the input
    isn't important, it's the outcome that matters.
    
    Since a single test is so crazy fast, let's run it 2000 times. Why not?

--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -6,39 +6,13 @@ require_relative 'gigasecond'
 class GigasecondTest < MiniTest::Unit::TestCase
 
   def test_1
-    d = Date.new(2011, 4, 25)
-    expected = d + 11574
+    1000.times do |x|
+      random_date = Time.at(rand * Time.now.to_i).to_date
+      expected = random_date + 11574
 
-    gs = Gigasecond.new(d)
-    assert_equal expected, gs.date
-    assert_equal expected, d.gigaseconds_since(1)
+      gs = Gigasecond.new(random_date)
+      assert_equal expected, gs.date
+      assert_equal expected, random_date.gigaseconds_since(1)
+    end
   end
-
-  def test_2
-    d = Date.new(1977, 6, 13)
-    expected = d + 11574
-
-    gs = Gigasecond.new(d)
-    assert_equal expected, gs.date
-    assert_equal expected, d.gigaseconds_since(1)
-  end
-
-  def test_3
-    d = Date.new(1959, 7, 19)
-    expected = d + 11574
-
-    gs = Gigasecond.new(d)
-    assert_equal expected, gs.date
-    assert_equal expected, d.gigaseconds_since(1)
-  end
-
-  def test_yourself
-    your_birthday = Date.new(2000, 1, 1)
-    expected = your_birthday + 11574
-
-    gs = Gigasecond.new(your_birthday)
-    assert_equal expected, gs.date
-    assert_equal expected, your_birthday.gigaseconds_since(1)
-  end
-
 end
