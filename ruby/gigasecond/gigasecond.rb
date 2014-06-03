aa95ccde2a59ee36b7316bc8565c88e36d0f2f0f
Ian Whitney <whit0694@umn.edu>
2014-06-03 13:47:51 -0500

    Move to SimpleDelegator

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,12 +1,6 @@
-class Gigasecond
-  def initialize(start_date)
-    self.start_date = start_date
-  end
-
+require 'delegate'
+class Gigasecond < SimpleDelegator
   def date
-    start_date + 11574 #number of days in a gigasecond
+    __getobj__ + 11574 #number of days in a gigasecond
   end
-
-  private
-  attr_accessor :start_date
 end
