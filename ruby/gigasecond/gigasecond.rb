b7802feffd9496c6ab748144bef6ea5159d952b7
Ian Whitney <whit0694@umn.edu>
2014-06-03 14:01:34 -0500

    Create a gigaseconds_since Date method
    Following the Rails approach, let's make this a Date method. Updated the
    tests to test both the implementations

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -4,3 +4,9 @@ class Gigasecond < SimpleDelegator
     __getobj__ + 11574 #number of days in a gigasecond
   end
 end
+
+class Date
+  def gigaseconds_since(multiple)
+    self + (11574 * multiple)
+  end
+end
--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -6,24 +6,39 @@ require_relative 'gigasecond'
 class GigasecondTest < MiniTest::Unit::TestCase
 
   def test_1
-    gs = Gigasecond.new(Date.new(2011, 4, 25))
-    assert_equal Date.new(2043, 1, 1), gs.date
+    d = Date.new(2011, 4, 25)
+    expected = d + 11574
+
+    gs = Gigasecond.new(d)
+    assert_equal expected, gs.date
+    assert_equal expected, d.gigaseconds_since(1)
   end
 
   def test_2
-    gs = Gigasecond.new(Date.new(1977, 6, 13))
-    assert_equal Date.new(2009, 2, 19), gs.date
+    d = Date.new(1977, 6, 13)
+    expected = d + 11574
+
+    gs = Gigasecond.new(d)
+    assert_equal expected, gs.date
+    assert_equal expected, d.gigaseconds_since(1)
   end
 
   def test_3
-    gs = Gigasecond.new(Date.new(1959, 7, 19))
-    assert_equal Date.new(1991, 3, 27), gs.date
+    d = Date.new(1959, 7, 19)
+    expected = d + 11574
+
+    gs = Gigasecond.new(d)
+    assert_equal expected, gs.date
+    assert_equal expected, d.gigaseconds_since(1)
   end
 
   def test_yourself
     your_birthday = Date.new(2000, 1, 1)
+    expected = your_birthday + 11574
+
     gs = Gigasecond.new(your_birthday)
-    assert_equal Date.new(2031, 9, 9), gs.date
+    assert_equal expected, gs.date
+    assert_equal expected, your_birthday.gigaseconds_since(1)
   end
 
 end
