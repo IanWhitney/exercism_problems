85ccc9b6f2db0245e00a581e92f23b985227b13b
Ian Whitney <whit0694@umn.edu>
2014-12-24 14:15:53 -0600

    Extract 'type' variable use out to classes
    Using 'type' as the parameter in `build_complement_for` is a flag that
    we need classes here, so this commit creates RNA and DNA classes.

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,32 +1,27 @@
 class Complement
   def self.of_dna(strand)
-    build_complement_for(strand, "dna")
+    build_complement_for(strand, DNA)
   end
 
   def self.of_rna(strand)
-    build_complement_for(strand, "rna")
+    build_complement_for(strand, RNA)
   end
 
   def self.build_complement_for(strand, type)
     ret = ""
-    strand.each_char { |x| ret << public_send("find_#{type}_complement_of".to_sym, x) }
+    strand.each_char { |x| ret << type.new(x).complement }
     ret
   end
+end
 
-  def self.find_dna_complement_of(nucleotide)
-    case nucleotide
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
-    end
+class RNA
+  attr_accessor :nucleotide
+
+  def initialize(nucleotide)
+    self.nucleotide = nucleotide
   end
 
-  def self.find_rna_complement_of(nucleotide)
+  def complement
     case nucleotide
     when 'C'
       'G'
@@ -36,6 +31,27 @@ class Complement
       'A'
     when 'A'
       'T'
+    end
+  end
+end
+
+class DNA
+  attr_accessor :nucleotide
+
+  def initialize(nucleotide)
+    self.nucleotide = nucleotide
+  end
+
+  def complement
+    case nucleotide
+    when 'C'
+      'G'
+    when 'G'
+      'C'
+    when 'T'
+      'A'
+    when 'A'
+      'U'
     end
   end
 end
